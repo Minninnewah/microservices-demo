fb08cb3f77e91d6fb5c275381a6fd7aad6cb08e7
heebzen1 <heebzen1@students.zhaw.ch>
2022-04-27 15:33:36 +0200

    Add evil IoT-handler

--- setup_port_forwarding.sh
+++ setup_port_forwarding.sh
@@ -1,2 +1,3 @@
 nohup microk8s kubectl port-forward -n chaos-testing service/chaos-dashboard  --address 10.161.2.161 2333:2333 & #Chaos dashboard
 nohup microk8s kubectl port-forward -n kube-system service/kubernetes-dashboard --address 10.161.2.161 10443:443 & #Kubernetes Dashboard
+nohup microk8s kubectl port-forward -n sock-shop service/iot-handler-evil --address 10.161.2.161 3002:3002 & #Evil IoT-handler
