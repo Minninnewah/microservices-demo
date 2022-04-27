236374da5ac08aa201f5cdb9d5b3c14fe2a99c1a
heebzen1 <heebzen1@students.zhaw.ch>
2022-04-27 13:49:03 +0200

    update environemnt setup

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+REQUIRED_PKG=jq
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
 sudo snap alias microk8s.kubectl kubectl
+
+echo "-----Delete chaos mesh-----"
 curl -sSL https://mirrors.chaos-mesh.org/v2.1.5/install.sh | bash -s -- --template | kubectl delete -f -
 kubectl patch crd/stresschaos.chaos-mesh.org -p '{"metadata":{"finalizers":[]}}' --type=merge
+
+echo "-----Delete sock-shop-----"
 microk8s.kubectl delete -f ./deploy/kubernetes/complete-demo.yaml
 
 namespaceStatus=$(kubectl get ns sock-shop -o json | jq .status.phase -r)
@@ -11,9 +23,10 @@ do
         echo "."
 done
 
+echo "-----Deploy sock-shop-----"
 microk8s.kubectl create namespace sock-shop
 microk8s.kubectl label namespace sock-shop istio-injection=enabled --overwrite
 microk8s.kubectl apply -f ./deploy/kubernetes/complete-demo.yaml
 
-
+echo "-----Deploy chaos mesh-----"
 curl -sSL https://mirrors.chaos-mesh.org/v2.1.5/install.sh | bash -s -- --microk8s
